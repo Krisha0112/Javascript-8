9dc4ca3aa5d06fc224bdf5a2bb39514f39260bcf
Krisha0112 <krishamangukiya0112@gmail.com>
2024-09-20 08:30:40 -0700

    Add files via upload

---
+++ lecture-2/lec-2.js
@@ -0,0 +1,10 @@
+var a = 10;
+// var b = 5;
+
+// console.log(typeof(a))
+
+console.log(`A = ${a}`)
+document.write(`<h1 style="color:red">A = ${a}</h1>`)
+alert(`A = ${a}`)
+document.getElementById('msg').innerHTML = `A = ${a}` 
+// document.getElementById('msg').innerHTML = `Sum = ${a+b}` 
